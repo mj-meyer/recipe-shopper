f6cf25de1164b3787e290ca340e2a6366c9dbdd1
MJ Meyer <mjmeyer@infoslips.com>
2018-07-09 20:50:48 +0200

    added recipe service and removed the event 'spaghetti' that was made redundant by the service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core';
+
 import { Recipe } from './recipes-list/recipe.model';
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'A test recipe description', 'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg'),
