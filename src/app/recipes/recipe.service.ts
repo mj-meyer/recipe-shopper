946964d4acc9c70916e922237866babf1a5dd807
MJ Meyer <mjmeyer@infoslips.com>
2018-08-16 12:38:10 +0200

    Setup prettier with tslint

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -6,14 +6,19 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'A test recipe description', 'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg'),
-    new Recipe('Another Test Recipe',
+    new Recipe(
+      'A Test Recipe',
+      'A test recipe description',
+      'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
       'A test recipe description that is much much longer',
-      'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg')
+      'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg'
+    )
   ];
 
   getRecipes() {
     return this.recipes.slice();
   }
-
 }
