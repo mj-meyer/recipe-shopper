caa19101af3c5f69fcabed75f1e4d21230cad510
MJ Meyer <mjmeyer@infoslips.com>
2018-08-16 13:06:09 +0200

    add ingredients to detail list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,23 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipes-list/recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'A test recipe description',
-      'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg'
+      'Cheese Burger',
+      'Ground beef with cheese, bbq sauce and baby potatoes',
+      'https://www.valdemarsro.dk/wp-content/2015/07/opskrift-aussie-burger.jpg',
+      [new Ingredient('Meat', 3), new Ingredient('Cheese', 2), new Ingredient('Baby potatoes', 9)]
     ),
     new Recipe(
-      'Another Test Recipe',
-      'A test recipe description that is much much longer',
-      'https://i.ndtvimg.com/i/2018-02/chicken-recipe_620x330_81517813176.jpg'
+      'Steak wrap',
+      'A delicious steak wrap with corn',
+      'https://www.seriouseats.com/2018/08/20180806-grilled-steak-recipes-roundup-05-1500x1125.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('Corn', 1), new Ingredient('Wrap', 1)]
     )
   ];
 
